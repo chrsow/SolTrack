801bd11d1d5a4f50d328dbbd013f07a9b4b47736
chrsow <liang.wasin@gmail.com>
2021-12-02 18:20:37 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# SolTrack
+
+A tool to find potential developer of a Solana program.
+
+## Background
+
+At the fundamental level, Solana programs are just variantion of eEBPF programs which are compiled from the Rust compiler. By default, the programs contain paths which also contains username of the machine you used to compile it. There were efforts like [this pre-RFC](https://github.com/rust-lang/rfcs/pull/3127) to remove the username by default, but it is not implemented at the moment. The following is an example that the home directory and username are leaked in a compiled program.
+
+```bash
+$ strings your-compiled-program.so
+
+# ...
+# /<your-home-directory>/<your-username>/.cargo/registry/src/github.com-1ecc6299db9ec823/solana-program-1.7.11/src/account_info.rs
+# ...
+```
+
+If you follow official documentations on Solana or a framework like [Anchor](https://github.com/project-serum/anchor) for building programs, chances are your identity is leaked in the programs.
+
+This is a [well known issue](https://www.bleepingcomputer.com/news/security/most-loved-programming-language-rust-sparks-privacy-concerns/) for several years and it is actually fine as long as you have no reason to hide your identity. However, in case you want to stay anonymous, with the fact that your programs contain your username and Solana programs are publicly accessible by anyone on the internet, your programs might leak your identity if your username can be used to identify you (which in most cases it could).
+
+In the meantime, to strip a username in programs, we can use `--remap-path-prefix` flag to map the default path prefix to something else.
+
+For example, we can add the flag like the following.
+```
+RUSTFLAGS=--remap-path-prefix=/<your-home-directory>/<my-username>=src cargo build-bpf
+```
+
+Or if you use Anchor framework.
+```
+RUSTFLAGS=--remap-path-prefix=/<your-home-directory>/<my-username>=src anchor build
+```
+
+Alternatively, you can consider to build your program with a separated CI/CD machine (i.e., Github Actions which also supports private repo, etc.).
+
+## Install
+```
+cargo install soltrack
+```
+
+## Build from source
+```
+cargo build
+```
+The compiled binary will be at `./target/debug/soltrack`.
+
+```bash
+$ ./target/debug/soltrack -h
+
+# SolTrack 1.0
+# Wasin Sae-ngow <https://github.com/chrsow>
+# Track potential developer of a Solana program
+
+# USAGE:
+#     soltrack [OPTIONS] <PROGRAM_ID>
+# ...
+```
+
+## Usage
+```
+soltrack <PROGRAM_ID> --network <NETWORK>
+```
+
+For example, if you want to find who is the developer of the native [Memo program](https://spl.solana.com/memo) in the Solana devnet. You can run the following command.
+
+```bash
+$ soltrack MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr --network devnet
+
+# [+] The program MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr on mainnet is deployed by: tyeraeulberg
+```
+As you can see, we have obtained the developer of the Memo program is "tyeraeulberg" who is one of the engineers of Solana.
---
+++ src/main.rs
@@ -0,0 +1,103 @@
+use bs58;
+use clap::{App, Arg};
+use regex::Regex;
+use solana_client::rpc_client::RpcClient;
+use solana_sdk::pubkey::Pubkey;
+use std::{collections::HashMap, time::Duration};
+
+const MAX_PROGRAM_ID_LEN: usize = 44;
+
+fn main() {
+    let matches = App::new("SolTrack")
+        .version("1.0")
+        .author("Wasin Sae-ngow <https://github.com/chrsow>")
+        .about("Track potential owner of a Solana program")
+        .arg(
+            Arg::with_name("program_id")
+                .value_name("PROGRAM_ID")
+                .required(true)
+                .index(1)
+                .help("Program ID to find."),
+        )
+        .arg(
+            Arg::with_name("network")
+                .short("n")
+                .long("network")
+                .value_name("NETWORK")
+                .takes_value(true)
+                .required(false)
+                .help(
+                    "Network of the programID. \
+                    [default: mainnet]",
+                ),
+        )
+        .get_matches();
+
+    // 0. Parse inputs
+    let program_id = matches.value_of("program_id").unwrap();
+    let network = matches.value_of("network").unwrap_or("mainnet");
+
+    if program_id.len() > MAX_PROGRAM_ID_LEN {
+        eprintln!("[-] Invalid ProgramID length");
+        std::process::exit(1);
+    }
+
+    let pubkey_vec = bs58::decode(program_id)
+        .into_vec()
+        .map_err(|_| panic!("Invalid Base58 string"))
+        .unwrap();
+    let account_pubkey = Pubkey::new(&pubkey_vec);
+
+    let url: &str;
+    match network {
+        "mainnet" => url = "https://api.mainnet-beta.solana.com",
+        "devnet" => url = "https://api.devnet.solana.com",
+        "testnet" => url = "https://api.testnet.solana.com",
+        "localhost" => url = "http://localhost:8899",
+        _ => {
+            eprintln!("[-] Invalid network (only supported 'mainnet', 'devnet', 'testnet' or 'localhost')");
+            std::process::exit(1);
+        }
+    }
+
+    // 1. Get a program executable content from the Solana chain
+    let rpc_timeout = Duration::from_secs(10);
+    let rpc_client = RpcClient::new_with_timeout(url.to_string(), rpc_timeout);
+    let account = rpc_client.get_account(&account_pubkey);
+    let account = match account {
+        Ok(_account) => _account,
+        Err(error) => {
+            eprintln!("[-] Couldn't get the account data: {}", error);
+            std::process::exit(1);
+        }
+    };
+    let is_executable = account.executable;
+
+    if !is_executable {
+        eprintln!("[-] The account does not contain an executable program");
+        std::process::exit(1);
+    }
+    let data = account.data.clone();
+    let binary_content = String::from_utf8_lossy(&data);
+
+    // 2. Find a username from the binary
+    let re = Regex::new(r"/(?P<username>[a-zA-Z0-9_-]*)/.cargo").unwrap();
+    let caps = re.captures(&binary_content).unwrap();
+    let owner: HashMap<&str, &str> = re
+        .capture_names()
+        .map(|o| o.and_then(|n| Some((n, caps.name(n)?.as_str()))))
+        .flatten()
+        .collect();
+
+    let username = &owner["username"];
+    let username_len = username.chars().count();
+    if username_len > 0 {
+        println!(
+            "[+] The program {} on {} is deployed by: {}",
+            program_id, network, username
+        );
+    } else {
+        eprintln!("[-] Username not found");
+        std::process::exit(1);
+    }
+}
